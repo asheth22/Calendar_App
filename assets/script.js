6757652dbc970ec0908b5aa33416c19780a1c663
Anjali Sheth <anjali.sheth@otis.com>
2020-12-17 18:22:58 -0500

    Initial push

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+var now = dayjs();
+var container = $(".container");
+
+setPlanner();
+
+$(".saveBtn").on('click', function(){
+    var currentEl = $(this);
+    var parentIndex = parseInt(currentEl.parent().index());
+    console.log("parent index on click:", parentIndex);
+    var txtArea = currentEl.siblings(".description");
+    localStorage.setItem(parentIndex, txtArea.val());
+    console.log(txtArea.val()); 
+})
+function setPlanner() {
+    $("#currentDay").text(now.format("dddd, MMMM D YYYY"));
+
+    container.children().each(function () {
+        var element = $(this);
+        var index = parseInt(element.index());
+        var pHourEl = element.children(".hour");
+        var pHour = dayjs().hour(8 + index);
+        pHour = pHour.format("h A");
+        pHourEl.html(pHour);
+        pHour = dayjs().hour(8 + index);
+        var hour = parseInt(pHour.format("H"));
+        var cHour = parseInt(dayjs().format("H"));
+        
+        console.log("current hour: ", cHour, "pHour: ", hour);
+        descEl = element.children(".description");
+        if (hour < cHour) {
+            console.log("planner time", hour, "is less than current hour", cHour);
+            descEl.css("background-color", "#d9d9d9");
+        }
+        else if (hour === cHour) {
+            console.log("planner time", hour, "is equal to current hour", cHour);
+            descEl.css("background-color", "#ff0000");
+        }
+        else {
+            console.log("planner time", hour, "is greater to current hour", cHour);
+            descEl.css("background-color", "#85e085");
+        }
+    });
+}
