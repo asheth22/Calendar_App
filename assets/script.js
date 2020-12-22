1f8b01cb0d1c701e6ed311e4e0fcf2d55b4b7ddf
Anjali Sheth <anjali.sheth@otis.com>
2020-12-22 17:10:30 -0500

    added timer to trigger auto update

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,31 @@
 var now = dayjs();
 var container = $(".container");
 var txtArea = "";
+var setInt;
+var initTime;
+var initTimeS;
+
 
 setPlanner();
+
+setInt = setInterval(interval, 1000);
+function interval() {
+    initTime = parseInt(dayjs().format("mm"));
+    initTimeS = parseInt(dayjs().format("ss"));
+    var minLeft = 60 - initTime;
+
+    if (initTime !== 0) {
+        console.log("initTime: ", initTime, " ", typeof (initTime), "minLeft: ", minLeft, " ", typeof (minLeft));
+    }
+    else {
+        console.log("Top of the hourr");
+        console.log("initTime: ", initTime, "initTimeS: ", initTimeS);
+        // location.reload(); 
+        if (initTimeS === 0) {
+            setPlanner();
+        }
+    }   
+}
 
 $(".saveBtn").on('click', function () {
     var currentEl = $(this);
@@ -13,36 +36,43 @@ $(".saveBtn").on('click', function () {
     console.log(txtArea.val());
 })
 function setPlanner() {
-    $("#currentDay").text(now.format("dddd, MMMM D YYYY"));
+          
+        now = dayjs();
+        $("#currentDay").text(now.format("dddd, MMMM D YYYY"));
 
-    container.children().each(function () {
-        var element = $(this);
-        var index = parseInt(element.index());
-        var pHourEl = element.children(".hour");
-        var txtEl = element.children(".description");
-        console.log(txtEl);
-        if (localStorage.getItem(index) !== null) {
-            // txtArea(HTML) = localStorage.setItem(index);
-            console.log("txtArea: ", txtEl);
-             txtEl.html(localStorage.getItem(index));
-        }
-        var pHour = dayjs().hour(9 + index);
-        pHour = pHour.format("h A");
-        pHourEl.html(pHour);
-        pHour = dayjs().hour(9 + index);
-        var hour = parseInt(pHour.format("H"));
-        var cHour = parseInt(dayjs().format("H"));
+        container.children().each(function () {
+            var element = $(this);
+            var index = parseInt(element.index());
+            var pHourEl = element.children(".hour");
+            var txtEl = element.children(".description");
+            console.log(txtEl);
+            if (localStorage.getItem(index) !== null) {
+                // txtArea(HTML) = localStorage.setItem(index);
+                console.log("txtArea: ", txtEl);
+                txtEl.html(localStorage.getItem(index));
+            }
+            var pHour = dayjs().hour(9 + index);
+            pHour = pHour.format("h A");
+            pHourEl.html(pHour);
+            pHour = dayjs().hour(9 + index);
+            var hour = parseInt(pHour.format("H"));
+            var cHour = parseInt(dayjs().format("H"));
 
-        console.log("current hour: ", cHour, "pHour: ", hour);
-        descEl = element.children(".description");
-        if (hour < cHour) {            
-            descEl.addClass("past");
-        }
-        else if (hour === cHour) {            
-            descEl.addClass("present");
-        }
-        else {            
-            descEl.addClass("future");
-        }
-    });
+            console.log("current hour: ", cHour, "pHour: ", hour);
+            descEl = element.children(".description");
+            if (hour < cHour) {
+                descEl.removeClass("present");
+                descEl.addClass("past");
+            }
+            else if (hour === cHour) {
+                descEl.removeClass("future");
+                descEl.addClass("present");
+            }
+            else {
+                descEl.addClass("future");
+            }
+            
+            return;
+        });
+    
 }
